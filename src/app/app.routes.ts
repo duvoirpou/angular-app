c20dddb8ba187ea863262debba1fc5849c1bb74e
Précieux Assako <assakoprecieux19@gmail.com>
2024-08-07 19:32:18 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /* { path: '', redirectTo: '/home', pathMatch: 'full' }, */ // Redirection par défaut
+  {path: 'crisis-list', component: CrisisListComponent},
+  {path: 'heroes-list', component: HeroesListComponent},
+];
